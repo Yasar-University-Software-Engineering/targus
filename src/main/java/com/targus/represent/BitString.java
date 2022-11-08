2f03ac19dd69f35b3f99b14c858c3171f1c377b6
Zafer Engin <efekanzafer.engin@gmail.com>
2022-11-09 01:13:03 +0300

    Define BitString class that implements Representation interface
    Add a field member of type BitSet for representation and implement clone function.

--- src/main/java/com/targus/represent/BitString.java
+++ src/main/java/com/targus/represent/BitString.java
@@ -2,9 +2,16 @@ package com.targus.represent;
 
 import com.targus.base.Representation;
 
+import java.util.BitSet;
+
 public class BitString implements Representation {
+    public BitSet bitSet;
+    public BitString(BitSet bitSet) {
+        this.bitSet = bitSet;
+    }
+
     @Override
     public Representation clone() {
-        return null;
+        return new BitString(bitSet);
     }
 }
