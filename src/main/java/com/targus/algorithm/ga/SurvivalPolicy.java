0ab7eeeacf7c865a932b5f76060d6ba13ace3d57
dogukanteber <dogukanteber1@hotmail.com>
2022-11-09 23:21:46 +0300

    Add SurvivalPolicy and SelectionPolicy interfaces

---
+++ src/main/java/com/targus/algorithm/ga/SurvivalPolicy.java
@@ -0,0 +1,11 @@
+package com.targus.algorithm.ga;
+
+import com.targus.base.OptimizationProblem;
+import com.targus.base.Solution;
+
+import java.util.List;
+
+public interface SurvivalPolicy {
+
+    List<Solution> apply(OptimizationProblem problem, List<Solution> solutions);
+}
