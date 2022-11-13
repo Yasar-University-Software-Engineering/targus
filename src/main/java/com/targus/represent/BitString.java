b47072d719e50abf8669d021178efef034e4728c
Zafer Engin <efekanzafer.engin@gmail.com>
2022-11-13 23:53:34 +0300

    Add the method "ones"
    This method returns a list of the indices of the ones in the field
    member of type BitSet.

--- src/main/java/com/targus/represent/BitString.java
+++ src/main/java/com/targus/represent/BitString.java
@@ -2,12 +2,23 @@ package com.targus.represent;
 
 import com.targus.base.Representation;
 
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 
 public class BitString implements Representation {
     public BitSet bitSet;
     public BitString(BitSet bitSet) {
         this.bitSet = bitSet;
+    }
+
+    public List<Integer> ones() {
+        List<Integer> ones= new ArrayList<>();
+        for (int i = 0; i < bitSet.size(); i++) {
+            if (bitSet.get(i))
+                ones.add(i);
+        }
+        return ones;
     }
 
     @Override
