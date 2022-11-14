55fdc274b600a3cfb052c8fbafabe7e59a23781b
dogukanteber <dogukanteber1@hotmail.com>
2022-11-14 18:01:33 +0300

    Forgot to add changes to OneBitMutation class

--- src/main/java/com/targus/algorithm/ga/OneBitMutation.java
+++ src/main/java/com/targus/algorithm/ga/OneBitMutation.java
@@ -3,11 +3,32 @@ package com.targus.algorithm.ga;
 import com.targus.base.OptimizationProblem;
 import com.targus.base.Solution;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
+import java.util.Random;
 
 public class OneBitMutation implements MutationOperator{
+    private final Random random;
+
+    public OneBitMutation() {
+        random = new SecureRandom();
+    }
+
     @Override
     public List<Solution> apply(OptimizationProblem problem, List<Solution> solutions) {
-        return null;
+        List<Solution> newSolutions = new ArrayList<>();
+        for (Solution s : solutions) {
+            if (random.nextDouble() < GA.MUTATION_PROBABILITY) {
+                BitSet individual = (BitSet) s.getRepresentation();
+                individual.flip(random.nextInt(individual.size()));
+                newSolutions.add((Solution) individual);
+            }
+            else {
+                newSolutions.add(s);
+            }
+        }
+        return newSolutions;
     }
 }
