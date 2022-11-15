59280b4089b7adf783448c7910208d322c055408
dogukanteber <dogukanteber1@hotmail.com>
2022-11-15 22:05:44 +0300

    Replace TreeMap with List and add sorting and getter for the best individual

--- src/main/java/com/targus/algorithm/ga/SimplePopulation.java
+++ src/main/java/com/targus/algorithm/ga/SimplePopulation.java
@@ -2,7 +2,6 @@ package com.targus.algorithm.ga;
 
 import com.targus.algorithm.BitStringSolution;
 import com.targus.base.OptimizationProblem;
-import com.targus.base.Representation;
 import com.targus.base.Solution;
 import com.targus.represent.BitString;
 
@@ -12,11 +11,11 @@ import java.util.*;
 public class SimplePopulation implements Population {
 
     private final int POPULATION_SIZE = 50;  // I do not think I should decide the population size
-    private TreeMap<Double, Solution> individuals;  // keeps the solution sorted
+    private List<Solution> individuals;  // keeps the solution sorted
     private Random random;
 
     public SimplePopulation() {
-        individuals = new TreeMap<>();
+        individuals = new ArrayList<>();
         random = new SecureRandom();
     }
 
@@ -24,8 +23,7 @@ public class SimplePopulation implements Population {
     public void init(OptimizationProblem problem) {
         for (int i = 0; i < POPULATION_SIZE; i++) {
             BitString random = generateRandomIndividual();
-            BitStringSolution randomSolution = new BitStringSolution(random, problem.objectiveValue(random));
-            individuals.put(randomSolution.objectiveValue(), randomSolution);
+            individuals.add(new BitStringSolution(random, problem.objectiveValue(random)));
         }
     }
 
@@ -40,7 +38,7 @@ public class SimplePopulation implements Population {
 
     @Override
     public void add(OptimizationProblem problem, Solution solution) {
-        individuals.put(problem.objectiveValue((Representation) solution), solution);
+        individuals.add(solution);
     }
 
     @Override
@@ -51,13 +49,21 @@ public class SimplePopulation implements Population {
     }
 
     @Override
-    public List<Solution> getIndividuals() {
-        return new ArrayList<>(individuals.values());
+    public void sortIndividuals() {
+        individuals.sort(Comparator.comparingDouble(Solution::objectiveValue));
     }
 
     @Override
-    public int getPopulationSize() {
-        return individuals.size();
+    public Solution getBestIndividual() {
+        if (individuals.isEmpty()) {
+            return null;
+        }
+        return individuals.get(0);  // return 0th element since the list will always be sorted
+    }
+
+    @Override
+    public List<Solution> getIndividuals() {
+        return individuals;
     }
 
     @Override
