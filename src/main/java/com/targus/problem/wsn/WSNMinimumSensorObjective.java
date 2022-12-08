a0db84c01f31b84994819c88f23fc0a30f2294ac
Zafer Engin <efekanzafer.engin@gmail.com>
2022-12-08 08:10:30 +0300

    Objective value was redesigned
    Objective value function was redesigned. It is now of type maximization
    and follows closely the one fitness model in the research paper.

--- src/main/java/com/targus/problem/wsn/WSNMinimumSensorObjective.java
+++ src/main/java/com/targus/problem/wsn/WSNMinimumSensorObjective.java
@@ -11,9 +11,9 @@ import java.util.List;
 
 public class WSNMinimumSensorObjective implements ObjectiveFunction {
 
-    public final static double weightSensor = 1;
-    public final static double weightMComm = 10;
-    public final static double weightKCov = 10;
+    public final static double weightSensor = 0.1;
+    public final static double weightMComm = 0.45;
+    public final static double weightKCov = 0.45;
 
     @Override
     public double value(ProblemModel model, Representation r) {
@@ -24,40 +24,44 @@ public class WSNMinimumSensorObjective implements ObjectiveFunction {
         List<Integer> sensors = bitString.ones();
 
         double sensorPenValueScaled = wsn.getPopulationSize() != 0 ?
-                (double) bitSet.cardinality() / wsn.getPopulationSize() : 0;
+                1 - ((double) bitSet.cardinality() / wsn.getPopulationSize()) : 0;
 
         double mConnPenValueScaled = sensors.size() * wsn.getM() != 0 ?
                 (double) mConnPenSum(wsn, sensors) / (sensors.size() * wsn.getM()) : 0;
 
         double kCoverPenValueScaled = wsn.targetsSize() * wsn.getK() != 0 ?
-                (double) kCovPenSum(wsn, sensors) / (wsn.targetsSize() * wsn.getK()) : 0;
+                (double) kCovPenSum(wsn, sensors) / (wsn.targetsSize() * wsn.getK()) : 1;
 
         return sensorPenValueScaled * weightSensor + mConnPenValueScaled * weightMComm + kCoverPenValueScaled * weightKCov;
     }
 
-    private int mConnPenSum(WSN wsn, List<Integer> sensors) {
+    public int mConnPenSum(WSN wsn, List<Integer> sensors) {
         int penSum = 0;
         int m = wsn.getM();
 
         for (Integer sensor:sensors)
         {
             int value = wsn.mConnSensors(sensor, sensors);
-            if (value < m) {
-                penSum += m - value;
+            if (value >= m) {
+                penSum += m;
+            } else {
+                penSum += value;
             }
         }
 
         return penSum;
     }
 
-    private int kCovPenSum(WSN wsn, List<Integer> sensors) {
+    public int kCovPenSum(WSN wsn, List<Integer> sensors) {
         int penSum = 0;
         int k = wsn.getK();
 
         for (int i = 0; i < wsn.targetsSize(); i++) {
             int value = wsn.kCovTargets(i, sensors);
-            if (value < k) {
-                penSum += k - value;
+            if (value >= k) {
+                penSum += k;
+            } else {
+                penSum += value;
             }
         }
 
