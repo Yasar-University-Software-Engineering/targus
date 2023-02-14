014f2a21571acff8ba0f8dc05c2edb9cb17a5c4c
dogukanteber <dogukanteber1@hotmail.com>
2023-02-14 18:32:55 +0300

    Update Constants

--- src/main/java/com/targus/utils/Constants.java
+++ src/main/java/com/targus/utils/Constants.java
@@ -11,5 +11,7 @@ public class Constants {
     public static final String K = "k";
     public static final String MUTATION_RATE = "mutationRate";
     public static final String GENERATION_COUNT = "generationCount";
+    public static final String STANDARD_GA_EXPERIMENT_FILE_NAME = "standard_ga_benchmark_tests.txt";
+    public static final String IMPROVED_GA_EXPERIMENT_FILE_NAME = "improved_ga_benchmark_tests.txt";
 
 }
