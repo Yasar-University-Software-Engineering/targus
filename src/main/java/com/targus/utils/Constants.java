3ced4f404f09ebbb95162991d6fdfb3876fe3e87
dogukanteber <dogukanteber1@hotmail.com>
2023-02-21 18:37:21 +0300

    Update Constants class

--- src/main/java/com/targus/utils/Constants.java
+++ src/main/java/com/targus/utils/Constants.java
@@ -17,4 +17,9 @@ public class Constants {
     public static final int DEFAULT_POPULATION_COUNT = 60;
     public static final int DEFAULT_COMMUNICATION_RANGE = 100;
     public static final int DEFAULT_SENSING_RANGE = 50;
+    public static final double DEFAULT_MUTATION_RATE = 0.03;
+    public static final int DEFAULT_M = 1;
+    public static final int DEFAULT_K = 2;
+    public static final int DEFAULT_ITERATION_COUNT = 10000;
+    public static final int DEFAULT_TIME_IN_SECONDS = 10;
 }
