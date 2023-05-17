5aa9ec53d1e841910663d5253a6290ebe51f9dc3
Zafer Engin <efekanzafer.engin@gmail.com>
2023-05-17 14:50:58 +0300

    Add icon and make the title lowercase

--- src/main/java/com/targus/ui/Main.java
+++ src/main/java/com/targus/ui/Main.java
@@ -1,10 +1,14 @@
 package com.targus.ui;
 
+import com.targus.ui.widgets.Sensor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Main extends Application {
     private static Stage primaryStage;
@@ -24,12 +28,21 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/targus/main.fxml"));
         Parent root = loader.load();
 
-        Main.primaryStage.setTitle("Targus");
+        Main.primaryStage.setTitle("targus");
         Scene scene = new Scene(root, 900, 700);
         scene.getStylesheets().add("/css/styles.css");
 
         Main.primaryStage.setScene(scene);
         Main.primaryStage.setMaximized(true);
         Main.primaryStage.show();
+
+        Image logoImage = new Image(Objects.requireNonNull(getClass().getResource("/icons/logo.png")).toExternalForm());
+        primaryStage.getIcons().add(logoImage);
     }
+
+    @Override
+    public void stop() {
+        Sensor.clearSensorArrayList();
+    }
+
 }
